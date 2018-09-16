db1309012b98df6c5662d65f756cd3e57a9e4636
Simolx <lx.simon@yahoo.com>
2018-09-16 17:35:37 +0800

    add mysql conf for mysql-5.7+, add pong to db in airflow-daemon, remove supervisor config

--- airflow/bin/airflow-daemon.sh
+++ airflow/bin/airflow-daemon.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+db_connected=1
+while [ "$db_connected" != "0" ]; do
+	ping -c 3 db
+	db_connected=$?
+done
+
 sleep 30
 tables=$(mysql -u airflow -p'airflow' -h ${AIRFLOW_METASTORE} -e 'select table_name from information_schema.tables where table_schema="airflow";')
 wordsnum=$(echo ${tables} | wc -w)
