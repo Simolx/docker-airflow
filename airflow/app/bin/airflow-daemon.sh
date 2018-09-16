a881a265d7c8a3d179681cd77de8955df5914dad
Simolx <lx.simon@yahoo.com>
2018-09-16 21:19:29 +0800

    remove command in docker-compose.yml, add container and services check in airflow-daemon.sh

--- airflow/app/bin/airflow-daemon.sh
+++ airflow/app/bin/airflow-daemon.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-db_connected=1
+ping -c 3 db
+db_connected=$?
 while [ "$db_connected" != "0" ]; do
+	echo "sleep 3 seconds for db container up"
+	sleep 3
 	ping -c 3 db
 	db_connected=$?
 done
 
-sleep 30
 tables=$(mysql -u airflow -p'airflow' -h ${AIRFLOW_METASTORE} -e 'select table_name from information_schema.tables where table_schema="airflow";')
+while [ "$?" != "0" ]; do
+	echo "sleep 5 seconds for db service start up"
+	sleep 5
+    tables=$(mysql -u airflow -p'airflow' -h ${AIRFLOW_METASTORE} -e 'select table_name from information_schema.tables where table_schema="airflow";')
+done
+
 wordsnum=$(echo ${tables} | wc -w)
 if [ ${wordsnum} -eq 0 ]; then
 	echo 'database airflow is empty, run airflow initdb'
