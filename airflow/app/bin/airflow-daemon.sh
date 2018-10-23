079d53e05d2bf3921176acf65bc7e4ac741bf17e
Simolx <lx.simon@yahoo.com>
2018-10-23 23:04:42 +0800

    fix bug when airflow use host network, mysql in unreachable

--- airflow/app/bin/airflow-daemon.sh
+++ airflow/app/bin/airflow-daemon.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-ping -c 3 db
+ping -c 3 ${AIRFLOW_METASTORE}
 db_connected=$?
 while [ "$db_connected" != "0" ]; do
-	echo "sleep 3 seconds for db container up"
+	echo "sleep 3 seconds for db container ${AIRFLOW_METASTORE} up"
 	sleep 3
-	ping -c 3 db
+	ping -c 3 ${AIRFLOW_METASTORE}
 	db_connected=$?
 done
 
-tables=$(mysql -u airflow -p'airflow' -h ${AIRFLOW_METASTORE} -e 'select table_name from information_schema.tables where table_schema="airflow";')
+tables=$(mysql -u airflow -p'airflow' -h ${AIRFLOW_METASTORE} -P ${AIRFLOW_METASTORE_PORT:-3306} -e 'select table_name from information_schema.tables where table_schema="airflow";')
 while [ "$?" != "0" ]; do
 	echo "sleep 5 seconds for db service start up"
 	sleep 5
-    tables=$(mysql -u airflow -p'airflow' -h ${AIRFLOW_METASTORE} -e 'select table_name from information_schema.tables where table_schema="airflow";')
+    tables=$(mysql -u airflow -p'airflow' -h ${AIRFLOW_METASTORE} -P ${AIRFLOW_METASTORE_PORT:-3306} -e 'select table_name from information_schema.tables where table_schema="airflow";')
 done
 
 wordsnum=$(echo ${tables} | wc -w)
@@ -22,3 +22,4 @@ if [ ${wordsnum} -eq 0 ]; then
 	airflow initdb
 fi
 airflow webserver -p 8081
+
